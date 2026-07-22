8e5f5a79d842ddbac9cc876433f9fff0b7438b09
serif145-tech <serif145-tech@users.noreply.github.com>
2026-07-23 03:33:35 +0700

    Open iPhone voice settings from the app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'devran-kuyumculuk-v11';
+const CACHE = 'devran-kuyumculuk-v12';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './brand.svg', './diamond.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
